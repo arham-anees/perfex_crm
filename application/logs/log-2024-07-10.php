6762a4c50aacdff5be81e89a98cc9c1b675c061c
omais-ahmad <moonmalikunited18@gmail.com>
2024-07-10 16:31:02 +0500

    added list

--- application/logs/log-2024-07-10.php
+++ application/logs/log-2024-07-10.php
@@ -52,3 +52,22 @@ ERROR - 2024-07-10 12:05:00 --> Could not find the language line "appointments"
 ERROR - 2024-07-10 09:05:04 --> 404 Page Not Found: ../../modules/appointly/controllers//index
 ERROR - 2024-07-10 09:05:21 --> 404 Page Not Found: ../../modules/appointly/controllers//index
 ERROR - 2024-07-10 09:09:53 --> 404 Page Not Found: ../../modules/appointly/controllers//index
+ERROR - 2024-07-10 13:00:04 --> Severity: Warning --> include_once(C:\xampp\htdocs\perfex_appointly\application\hooks./../libraries/App_modules.php): Failed to open stream: Permission denied C:\xampp\htdocs\perfex_appointly\application\hooks\InitModules.php 12
+ERROR - 2024-07-10 13:00:04 --> Severity: Warning --> include_once(): Failed opening 'C:\xampp\htdocs\perfex_appointly\application\hooks./../libraries/App_modules.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\perfex_appointly\application\hooks\InitModules.php 12
+ERROR - 2024-07-10 13:00:04 --> Severity: Warning --> include_once(C:\xampp\htdocs\perfex_appointly\application\hooks./../../system/helpers/directory_helper.php): Failed to open stream: Permission denied C:\xampp\htdocs\perfex_appointly\application\hooks\InitModules.php 14
+ERROR - 2024-07-10 13:00:04 --> Severity: Warning --> include_once(): Failed opening 'C:\xampp\htdocs\perfex_appointly\application\hooks./../../system/helpers/directory_helper.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\perfex_appointly\application\hooks\InitModules.php 14
+ERROR - 2024-07-10 13:00:04 --> Severity: error --> Exception: Class "App_modules" not found C:\xampp\htdocs\perfex_appointly\application\hooks\InitModules.php 16
+ERROR - 2024-07-10 13:01:26 --> Severity: error --> Exception: Unknown database 'perfex_crm' C:\xampp\htdocs\perfex_appointly\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2024-07-10 16:07:16 --> Severity: error --> Exception: Table 'perfex_crm.tblappointly_booking_pages' doesn't exist C:\xampp\htdocs\perfex_crm\system\database\drivers\mysqli\mysqli_driver.php 307
+ERROR - 2024-07-10 16:08:48 --> Severity: Warning --> Undefined array key 0 C:\xampp\htdocs\perfex_crm\modules\appointly\views\booking_pages\booking_pages.php 9
+ERROR - 2024-07-10 16:08:48 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\perfex_crm\modules\appointly\views\booking_pages\booking_pages.php 9
+ERROR - 2024-07-10 16:13:49 --> Severity: Warning --> Undefined array key 0 C:\xampp\htdocs\perfex_crm\modules\appointly\views\booking_pages\booking_pages.php 9
+ERROR - 2024-07-10 16:13:49 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\perfex_crm\modules\appointly\views\booking_pages\booking_pages.php 9
+ERROR - 2024-07-10 13:14:24 --> 404 Page Not Found: ../../modules/appointly/controllers/Appointments_public/logo.png
+ERROR - 2024-07-10 16:23:59 --> Could not find the language line "Name"
+ERROR - 2024-07-10 16:23:59 --> Could not find the language line "Options"
+ERROR - 2024-07-10 16:25:57 --> Could not find the language line "Name"
+ERROR - 2024-07-10 16:25:57 --> Could not find the language line "Description"
+ERROR - 2024-07-10 16:25:57 --> Could not find the language line "Url"
+ERROR - 2024-07-10 16:26:53 --> Could not find the language line "general"
+ERROR - 2024-07-10 16:26:53 --> Could not find the language line "form"
--- modules/appointly/views/booking_pages/booking_pages.php
+++ modules/appointly/views/booking_pages/booking_pages.php
@@ -1,21 +1,41 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
-<?php init_head(); 
-?><div id="wrapper">
-<div class="content">
-   <div class="row">
-      <div class="col-md-12">
-         <div class="panel_s">
-            <div class="panel-body">
-           <?php echo $booking_pages[0]['url'] ?>
-           
+<?php init_head(); ?>
+<div id="wrapper">
+    <div class="content">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel_s">
+                    <div class="panel-body panel-table-full">
+                        <table class="table table-statuses">
+                            <thead>
+                                <tr>
+                                    <th><?php echo _l('Name'); ?></th>
+                                    <th><?php echo _l('Description'); ?></th>
+                                    <th><?php echo _l('Url'); ?></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php if (!empty($booking_pages)): ?>
+                                    <?php foreach ($booking_pages as $page): ?>
+                                        <tr>
+                                            <td><?php echo $page['name']; ?></td>
+                                            <td><?php echo $page['description']; ?></td>
+                                            <td><?php echo $page['url']; ?></td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                                <?php else: ?>
+                                    <tr>
+                                        <td colspan="2"><?php echo _l('No booking pages found.'); ?></td>
+                                    </tr>
+                                <?php endif; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
-         </div>
-      </div>
-   </div>
-</div>
+        </div>
+    </div>
 </div>
 <?php init_tail(); ?>
-
-
 </body>
 </html>
