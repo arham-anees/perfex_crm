6d2a1a74426ad15e3c1c0fd33bf85dd20d198b6c
Abdur Rauf <raufabdur886@gmail.com>
2024-08-02 10:19:33 +0500

    create CRUD of prospects

--- modules/leadevo/models/Prospects_model.php
+++ modules/leadevo/models/Prospects_model.php
@@ -9,31 +9,26 @@ class Prospects_model extends CI_Model
         parent::__construct();
     }
 
-    // Get all prospects
     public function get_all()
     {
-        return $this->db->get($this->table)->result_array();
+        return $this->db->get($this->table)->result();
     }
 
-    // Get a single prospect by ID
     public function get($id)
     {
         return $this->db->where('id', $id)->get($this->table)->row();
     }
 
-    // Insert a new prospects
     public function insert($data)
     {
         return $this->db->insert($this->table, $data);
     }
 
-    // Update an prospect
     public function update($id, $data)
     {
         return $this->db->where('id', $id)->update($this->table, $data);
     }
 
-    // Delete an prospect
     public function delete($id)
     {
         return $this->db->where('id', $id)->delete($this->table);
