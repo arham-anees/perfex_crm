a3a79219ea2ed626d131f72ed8da9e16b35ef438
omais-ahmad <moonmalikunited18@gmail.com>
2024-08-08 16:49:44 +0500

    campaigns fix

--- application/models/leadevo/Campaigns_model.php
+++ application/models/leadevo/Campaigns_model.php
@@ -3,8 +3,9 @@
 class Campaigns_model extends CI_Model
 {
 
+    protected $table = 'tblleadevo_campaign'; // Define the table name
+    protected $country_table = 'tblcountries'; // Define the table name
 
-    protected $table = 'leadevo_campaign'; // Define the table name
 
     public function __construct()
     {
@@ -14,7 +15,7 @@ class Campaigns_model extends CI_Model
 
     public function get_all()
     {
-        return $this->db->get($this->table)->result_array();
+        return $this->db->get($this->table)->result();
     }
 
     public function get($id)
@@ -38,6 +39,11 @@ class Campaigns_model extends CI_Model
     }
     public function get_campaign_statuses()
     {
-        return $this->db->get('leadevo_campaign_statuses')->result_array();
+        return $this->db->get('tblleadevo_campaign_statuses')->result_array();
+    }
+
+    public function get_all_countries()
+    {
+        return $this->db->get($this->country_table)->result_array();
     }
 }
