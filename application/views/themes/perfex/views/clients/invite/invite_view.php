eea2cb05010911d727f09bfe59e4a4f12387af71
Abdur Rauf <raufabdur886@gmail.com>
2024-08-13 09:48:45 +0500

    show invite-view file

---
+++ application/views/themes/perfex/views/clients/invite/invite_view.php
@@ -0,0 +1,22 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+<div class="row">
+    <div class="content">
+        <div class="row main_row">
+            <div class="col-md-12">
+                <div class="panel_s">
+                    <div class="panel-body">
+                        <?php echo form_open(site_url('invite'), ['id' => 'invite-friend-form']); ?>
+                            <?php echo render_input('name', 'Name', '', 'text'); ?>
+                            <?php echo render_input('email', 'Email', '', 'email'); ?>
+                            <input type="submit" value="Invite" class="btn btn-primary"/>
+                        <?php echo form_close(); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Include JavaScript file using <script> tags -->
+<script src="<?= base_url('modules/leadevo/assets/js/invite/invite.js'); ?>"></script>
+<?php init_tail(); ?>
