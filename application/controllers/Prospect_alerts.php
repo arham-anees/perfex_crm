4eb13d7310176f27b9719e65ddd19cdd4a966243
Arham Anees <arhamanees10@gmail.com>
2024-08-29 11:57:44 +0500

    prospect alerts

--- application/controllers/Prospect_alerts.php
+++ application/controllers/Prospect_alerts.php
@@ -11,66 +11,71 @@ class Prospect_alerts extends ClientsController
 
     public function index()
     {
+        log_message('error', 'step1');
         $search = $this->input->get('search');
         $data['categories'] = $this->Prospect_categories_model->get_all();
+        log_message('error', 'step2');
         $filter = $this->input->get('filter');
         $data['alerts'] = $this->Prospect_alerts_model->get_all();
+        log_message('error', 'step3');
         $this->data($data);
         $this->view('clients/prospect_alerts/prospect_alerts');
         $this->layout();
     }
 
     public function create()
-{
-    if ($this->input->post()) {
-        $data = [
-            'name' => $this->input->post('name'),
-            'prospect_category_id' => $this->input->post('prospect_category_id'),
-            'email' => $this->input->post('email'),
-            'phone' => $this->input->post('phone'),
-        ];
+    {
+        if ($this->input->post()) {
+            $data = [
+                'name' => $this->input->post('name'),
+                'prospect_category_id' => $this->input->post('prospect_category_id'),
+                'email' => $this->input->post('email'),
+                'phone' => $this->input->post('phone'),
+                'status' => 0,
+                'is_exclusive' => (int) $this->input->post('is_exclusive'),
+            ];
 
-        // Inserting the data into tblleadevo_prospect_alerts
-        $this->Prospect_alerts_model->insert($data);
-        redirect('prospect_alerts');
-    } else {
-        // Fetch categories from tblleadevo_prospect_categories for the dropdown
-        $data['prospect_categories'] = $this->Prospect_categories_model->get_all();
-        
-        // Passing categories to the view
-        $this->data($data);
-        $this->view('clients/prospect_alerts/create');
-        $this->layout();
+            // Inserting the data into tblleadevo_prospect_alerts
+            $this->Prospect_alerts_model->insert($data);
+            redirect('prospect_alerts');
+        } else {
+            // Fetch categories from tblleadevo_prospect_categories for the dropdown
+            $data['prospect_categories'] = $this->Prospect_categories_model->get_all();
+
+            // Passing categories to the view
+            $this->data($data);
+            $this->view('clients/prospect_alerts/create');
+            $this->layout();
+        }
     }
-}
 
-public function edit($id)
-{
-    if ($this->input->post()) {
-        // Fetch and prepare the updated data
-        $data = [
-            'name' => $this->input->post('name'),
-            'prospect_category_id' => $this->input->post('prospect_category_id'),
-            'email' => $this->input->post('email'),
-            'phone' => $this->input->post('phone'),
-            'is_active' => $this->input->post('is_active') ? 1 : 0,
-        ];
+    public function edit($id)
+    {
+        if ($this->input->post()) {
+            // Fetch and prepare the updated data
+            $data = [
+                'name' => $this->input->post('name'),
+                'prospect_category_id' => $this->input->post('prospect_category_id'),
+                'email' => $this->input->post('email'),
+                'phone' => $this->input->post('phone'),
+                'is_exclusive' => (int) $this->input->post('is_exclusive')
+            ];
 
-        // Update the prospect alert in the database
-        $this->Prospect_alerts_model->update($id, $data);
-        redirect('prospect_alerts');
-    } else {
-        // Fetch the current prospect alert data
-        $data['alert'] = $this->Prospect_alerts_model->get($id);
-        // Fetch all categories for the dropdown
-        $data['prospect_categories'] = $this->Prospect_categories_model->get_all();
+            // Update the prospect alert in the database
+            $this->Prospect_alerts_model->update($id, $data);
+            redirect('prospect_alerts');
+        } else {
+            // Fetch the current prospect alert data
+            $data['alert'] = $this->Prospect_alerts_model->get($id);
+            // Fetch all categories for the dropdown
+            $data['prospect_categories'] = $this->Prospect_categories_model->get_all();
 
-        // Load the edit view with the current data
-        $this->data($data);
-        $this->view('clients/prospect_alerts/edit');
-        $this->layout();
+            // Load the edit view with the current data
+            $this->data($data);
+            $this->view('clients/prospect_alerts/edit');
+            $this->layout();
+        }
     }
-}
 
     public function delete($id)
     {
@@ -81,7 +86,25 @@ public function edit($id)
         }
         redirect('prospect_alerts');
     }
-    public function view($id)
+    public function activate($id)
+    {
+        if ($this->Prospect_alerts_model->activate($id)) {
+            set_alert('success', _l('leadevo_prospect_alert_activated'));
+        } else {
+            set_alert('danger', _l('leadevo_prospect_alert_activation_failed'));
+        }
+        redirect('prospect_alerts');
+    }
+    public function deactivate($id)
+    {
+        if ($this->Prospect_alerts_model->deactivate($id)) {
+            set_alert('success', _l('leadevo_prospect_alert_deactivated'));
+        } else {
+            set_alert('danger', _l('leadevo_prospect_alert_deactivation_failed'));
+        }
+        redirect('prospect_alerts');
+    }
+    public function details($id)
     {
         $data['alert'] = $this->Prospect_alerts_model->get($id);
         $this->data($data);
--- application/views/themes/perfex/views/clients/prospect_alerts/edit.php
+++ application/views/themes/perfex/views/clients/prospect_alerts/edit.php
@@ -8,45 +8,63 @@
                 <hr class="hr-panel-heading">
 
                 <?php echo form_open(site_url('prospect_alerts/edit/' . $alert['id']), ['method' => 'POST']); ?>
-                
+
                 <div class="row">
-                    <!-- First Column -->
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="name"><?php echo _l('Name'); ?></label>
-                            <input type="text" name="name" class="form-control" id="name" value="<?php echo set_value('name', $alert['name']); ?>">
+                    <div class="form-group">
+                        <label for="name"><?php echo _l('Name'); ?></label>
+                        <input type="text" name="name" class="form-control" id="name"
+                            value="<?php echo set_value('name', $alert['name']); ?>">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="prospect_category"><?php echo _l('Prospect Category'); ?></label>
+                        <select name="prospect_category_id" class="selectpicker" data-width="100%"
+                            data-none-selected-text="<?php echo _l('Select Prospect Category'); ?>">
+                            <option value=""><?php echo _l('Select Prospect Category'); ?></option>
+                            <?php foreach ($prospect_categories as $category): ?>
+                                <option value="<?php echo $category->id; ?>" <?php echo set_select('prospect_category_id', $category->id, $alert['prospect_category_id'] == $category->id); ?>>
+                                    <?php echo $category->name; ?>
+                                </option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="opt_1_prospect_type" class="control-label clearfix">
+                            <?php echo _l('leadevo_alerts_type'); ?>
+                        </label>
+                        <div class="radio radio-primary radio-inline">
+                            <input type="radio" id="opt_1_prospect_type" name="is_exclusive" value="1" <?php echo $alert['is_exclusive'] == 1 ? 'checked' : '' ?>>
+                            <label for="opt_1_prospect_type">
+                                <?php echo _l('leadevo_prospect_alert_exclusive') ?>
+                            </label>
                         </div>
-                        
-                        <div class="form-group">
-                            <label for="email"><?php echo _l('Email'); ?></label>
-                            <input type="text" name="email" class="form-control" id="email" value="<?php echo set_value('email', $alert['email']); ?>">
+                        <div class="radio radio-primary radio-inline">
+                            <input type="radio" id="opt_2_prospect_type" name="is_exclusive" value="0" <?php echo $alert['is_exclusive'] == 0 ? 'checked' : '' ?>>
+                            <label for="opt_2_prospect_type">
+                                <?php echo _l('leadevo_prospect_alert_non_exclusive'); ?>
+                            </label>
                         </div>
                     </div>
 
-                    <!-- Second Column -->
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="prospect_category"><?php echo _l('Prospect Category'); ?></label>
-                            <select name="prospect_category_id" class="selectpicker" data-width="100%" data-none-selected-text="<?php echo _l('Select Prospect Category'); ?>">
-                                <option value=""><?php echo _l('Select Prospect Category'); ?></option>
-                                <?php foreach ($prospect_categories as $category) : ?>
-                                    <option value="<?php echo $category->id; ?>" <?php echo set_select('prospect_category_id', $category->id, $alert['prospect_category_id'] == $category->id); ?>>
-                                        <?php echo $category->name; ?>
-                                    </option>
-                                <?php endforeach; ?>
-                            </select>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="phone"><?php echo _l('Phone'); ?></label>
-                            <input type="text" name="phone" class="form-control" id="phone" value="<?php echo set_value('phone', $alert['phone']); ?>">
-                        </div>
+                    <div class="form-group">
+                        <label for="email"><?php echo _l('Email'); ?></label>
+                        <input type="text" name="email" class="form-control" id="email"
+                            value="<?php echo set_value('email', $alert['email']); ?>">
                     </div>
+
+                    <div class="form-group">
+                        <label for="phone"><?php echo _l('Phone'); ?></label>
+                        <input type="text" name="phone" class="form-control" id="phone"
+                            value="<?php echo set_value('phone', $alert['phone']); ?>">
+                    </div>
+
                 </div>
 
                 <div class="form-group text-left">
                     <button type="submit" class="btn btn-primary"><?php echo _l('Save'); ?></button>
-                    <a href="<?php echo site_url('prospect_alerts'); ?>" class="btn btn-info"><?php echo _l('Cancel'); ?></a>
+                    <a href="<?php echo site_url('prospect_alerts'); ?>"
+                        class="btn btn-info"><?php echo _l('Cancel'); ?></a>
                 </div>
                 </form>
             </div>
@@ -55,4 +73,5 @@
 </div>
 
 </body>
+
 </html>
