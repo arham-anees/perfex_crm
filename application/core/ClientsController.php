d5e4a77f8d5a48fcef1cc61c64bf2c1aec637b7b
Arham Anees <arhamanees10@gmail.com>
2024-09-02 13:32:23 +0500

    prospect status

--- application/core/ClientsController.php
+++ application/core/ClientsController.php
@@ -30,8 +30,8 @@ class ClientsController extends App_Controller
 
         // Deleted or inactive but have session
         if (!$currentUser || $currentUser->active == 0) {
-            $this->authentication_model->logout();
-            redirect(admin_url('authentication'));
+            //$this->authentication_model->logout();
+            //redirect(site_url('authentication'));
         }
 
         if (
