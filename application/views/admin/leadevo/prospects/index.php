1c489313d2aa0be856e89ebc9242e84ca78dd693
omais-ahmad <moonmalikunited18@gmail.com>
2024-09-03 12:58:10 +0500

    fixes

--- application/views/admin/leadevo/prospects/index.php
+++ application/views/admin/leadevo/prospects/index.php
@@ -145,36 +145,8 @@ function displayStars($rating, $maxStars = 5)
     </div>
 </div>
 <!-- View Prospect Modal -->
-<div class="modal fade" id="viewProspectModal" tabindex="-1" role="dialog" aria-labelledby="viewProspectModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="viewProspectModalLabel">Prospect Details</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <!-- Prospect data will be loaded here via AJAX -->
-                <div id="prospectDetails">
-                    <!-- Example Fields -->
-                    <p><strong>First Name:</strong> <span id="prospectFirstName"></span></p>
-                    <p><strong>Last Name:</strong> <span id="prospectLastName"></span></p>
-                    <p><strong>Status:</strong> <span id="prospectStatus"></span></p>
-                    <p><strong>Type:</strong> <span id="prospectType"></span></p>
-                    <p><strong>Acquisition Channel:</strong> <span id="prospectAcquisitionChannel"></span></p>
-                    <p><strong>Industry:</strong> <span id="prospectIndustry"></span></p>
-                    <p><strong>Activity log:</strong>
-                    <div id="activity-log"></div>
-                    </p>
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            </div>
-        </div>
-    </div>
+<div class="modal fade" id="viewProspectModal" tabindex="-1" role="dialog">
+<?php echo get_instance()->load->view('admin/leadevo/prospects/modals/view_prospect.php') ?>
 </div>
 
 <div id="mark_prospect_fake" class="modal fade" tabindex="-1" role="dialog">
@@ -390,6 +362,10 @@ function displayStars($rating, $maxStars = 5)
         document.querySelector('#send_via_campaign_modal input[name=id]').value = id;
         $('#send_via_campaign_modal').modal('show');
     }
+    function openViewModal(id) {
+        document.querySelector('#viewProspectModal input[name=id]').value = id;
+        $('#viewProspectModal').modal('show');
+    }
     function openRateModal(id) {
         document.querySelector('#rating_modal input[name=id]').value = id;
         $('#rating_modal').modal('show');
@@ -464,53 +440,7 @@ function displayStars($rating, $maxStars = 5)
     });
 </script>
 
-<script>
-    function openViewModal(prospectId) {
-        $.ajax({
-            url: '<?= admin_url('prospects/get_prospect_data') ?>',
-            type: 'GET',
-            data: { id: prospectId },
-            success: function (response) {
-                const res = JSON.parse(response);
-                const data = res.prospect;
-                const logs = res.logs ?? [];
 
-                // Populate the modal with the first name, last name, and other details
-                $('#prospectFirstName').text(data.first_name);
-                $('#prospectLastName').text(data.last_name);
-                $('#prospectStatus').text(data.status);
-                $('#prospectType').text(data.type);
-
-                $('#prospectAcquisitionChannel').text(data.acquisition_channel);
-                $('#prospectIndustry').text(data.industry);
-
-                let logsHtml = '';
-                for (let i = 0; i < logs.length; i++) {
-                    const log = logs[i];
-                    let type = '';
-                    let name = log.staff_name;
-                    if (name == null || name == '') name = log.client_name;
-                    let date = log.date;
-                    if (log.type == 'marked_fake') type = 'Marked as Fake';
-                    if (log.type == 'remove_from_market') type = 'Remove from Marketplace';
-                    if (log.type == 'auto_deliverable') type = 'Deliver Automatically';
-
-
-                    logsHtml += `<div class='prospect-log-item'>${name} at ${date} - ${type}`
-                    if (log.comments != null && log.comments != '') { logsHtml += ` - ${log.comments}`; }
-                    logsHtml += '</div>'
-                }
-                $('#activity-log').html(logsHtml);
-
-                // Show the modal
-                $('#viewProspectModal').modal('show');
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                alert('Failed to fetch prospect data: ' + errorThrown);
-            }
-        });
-    }
-</script>
 
 </body>
 
