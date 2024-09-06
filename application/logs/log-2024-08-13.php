fceb9f9f6e2c371458db435a8d883a1ac383661a
Arham Anees <arhamanees10@gmail.com>
2024-09-06 07:51:03 +0500

    logs removed

--- application/logs/log-2024-08-13.php
+++
@@ -1,8 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2024-08-13 17:40:44 --> client
-ERROR - 2024-08-13 17:40:44 --> 12
-ERROR - 2024-08-13 17:40:44 --> client
-ERROR - 2024-08-13 17:40:48 --> client
-ERROR - 2024-08-13 17:40:48 --> 12
-ERROR - 2024-08-13 17:40:48 --> client
